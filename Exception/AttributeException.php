db6069709bda10ac7df937b11d82119a40029415
Mesut Vatansever <mesut.vatansever@freebirdairlines.com>
2016-09-21 17:17:08 +0300

    Added attribute based actions exceptions

---
+++ Exception/AttributeException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Mesut Vatansever | mesut.vatansever[at]freebirdairlines.com
+ * Date: 21/09/16 16:56
+ */
+
+namespace Symfony\Component\Ldap\Exception;
+
+class AttributeException extends \RuntimeException
+{
+
+}
